2034407e3f1cfa8f7c4e88f95b303109fdba2916
Tomo Krajina <tkrajina@gmail.com>
2012-08-01 10:58:40 +0200

    Pull push inbound messages

--- oneapi/__init__.py
+++ oneapi/__init__.py
@@ -154,6 +154,25 @@ class SmsClient(AbstractOneApiClient):
 
         return mod_object.Conversions.from_json(mod_models.DeliveryInfoList, result, not is_success)
 
+    def retrieve_inbound_messages(max_number):
+        if not max_number or max_number < 0:
+            max_number = 100
+
+        params = {
+                'maxBatchSize': max_number,
+        }
+
+        is_success, result = self.execute_GET(
+                '/1/smsmessaging/inbound/registrations/INBOUND/messages', 
+                params
+        )
+
+        return self.create_from_json(mod_object.InboundSmsMessages, result, not is_success)
+
+    @staticmethod
+    def unserialize_inbound_message(json):
+        return mod_object.Conversions.from_json(mod_object.InboundSmsMessages, json, False)
+
 class DataConnectionProfileClient(AbstractOneApiClient):
 
     def __init__(self, username, password, base_url=None):
