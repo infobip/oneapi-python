c3fb51ec729938604cfae9e87bd0f6bf0cbcc63a
jiceher <jiceher@free.fr>
2013-09-11 09:08:36 +0200

    Update send message example with JSON support

--- example_send_message_and_check_status.py
+++ example_send_message_and_check_status.py
@@ -12,13 +12,21 @@ import oneapi.dummyserver as dummyserver
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
 
-if len(sys.argv) < 4:
-    print 'Please enter username, password and address'
+if len(sys.argv) < 6:
+    print 'Please enter username, password, address, sender, content-type (json|url), [accept-type]'
     sys.exit(1)
 
 username = sys.argv[1]
 password = sys.argv[2]
 address = sys.argv[3]
+sender = sys.argv[4]
+data_format = sys.argv[5]
+
+if len(sys.argv) == 7:
+    accept = sys.argv[6]
+
+if (data_format != "json"):
+    data_format = "url"
 
 # example:initialize-sms-client
 sms_client = oneapi.SmsClient(username, password)
@@ -26,14 +34,19 @@ sms_client = oneapi.SmsClient(username, password)
 
 # example:prepare-message-without-notify-url
 sms = models.SMSRequest()
-sms.sender_address = address
+sms.sender_address = sender
 sms.address = address
 sms.message = 'Test message'
 sms.callback_data = 'Any string'
+sms.notify_url = 'Any URL'
 # ----------------------------------------------------------------------------------------------------
 
+header = None
+if 'accept' in locals():
+    header = {"accept" : accept}
+
 # example:send-message
-result = sms_client.send_sms(sms)
+result = sms_client.send_sms(sms, header, data_format)
 # store client correlator because we can later query for the delivery status with it:
 client_correlator = result.client_correlator
 # ----------------------------------------------------------------------------------------------------
