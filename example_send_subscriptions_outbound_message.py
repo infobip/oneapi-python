2374ef1c3fdb7b040266370efa81babdeb98312c
jiceher <jiceher@free.fr>
2013-09-11 09:09:50 +0200

    Add example for subscribe/unsubscribe to notifications

---
+++ example_send_subscriptions_outbound_message.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+import pdb
+
+import sys as sys
+import logging as logging
+import time as time
+
+import oneapi as oneapi
+import oneapi.models as models
+import oneapi.dummyserver as dummyserver
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+
+if len(sys.argv) < 5:
+    print 'Please enter username, password, dest, source, content-type (url|json), [accept-type]'
+    sys.exit(1)
+
+username = sys.argv[1]
+password = sys.argv[2]
+sender = sys.argv[3]
+data_format = sys.argv[4]
+
+if len(sys.argv) == 6:
+   accept = sys.argv[5]
+
+if data_format != "json":
+   data_format = "url"
+
+# example:initialize-sms-client
+sms_client = oneapi.SmsClient(username, password)
+# ----------------------------------------------------------------------------------------------------
+
+# example:prepare-message-without-notify-url
+sms = models.SMSRequest()
+sms.sender_address = sender
+sms.notify_url = 'Any URL'
+sms.callback_data = 'Any string'
+sms.filter_criteria = "Urgent"
+# ----------------------------------------------------------------------------------------------------
+
+header=None
+if 'accept' in locals():
+   header = {"accept" : accept}
+
+# example:send-message
+result = sms_client.subscribe_delivery_status(sms, header, data_format)
+# store client correlator because we can later query for the delivery status with it:
+resource_url = result.resource_url
+# ----------------------------------------------------------------------------------------------------
+
+if not result.is_success():
+    print 'Error sending message:', result.exception
+    sys.exit(1)
+
+print 'Is success = ', result.is_success()
+print 'Resource URL = ', result.resource_url
+
+# Few seconds later we can delete subscription
+time.sleep(10)
+
+# example:query-for-delivery-status
+sms_client.delete_delivery_status_subscription(resource_url, header, data_format)
+# ----------------------------------------------------------------------------------------------------
